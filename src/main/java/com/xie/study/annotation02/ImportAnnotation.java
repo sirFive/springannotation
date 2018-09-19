d43dd087cc833ff82ba6f8048f4bd2ff9fd7e66a
xie <858381526@qq.com>
2018-09-19 17:34:15 +0800

    修改注释

--- src/main/java/com/xie/study/annotation02/ImportAnnotation.java
+++ src/main/java/com/xie/study/annotation02/ImportAnnotation.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 /**
  * Created by Administrator on 2018/9/16.
  * 给容器注册组建的方式：
- * 1.包扫描+组件标注注解（@Color,@Service,@Component,@Repository）
+ * 1.包扫描+组件标注注解（@Controller,@Service,@Component,@Repository）
  * 2.@Bean(一般用于引入第三方包里面的组件注册)
  * 3.@import快速给容器导入一个组件（id是导入组件类的全名）
  */
