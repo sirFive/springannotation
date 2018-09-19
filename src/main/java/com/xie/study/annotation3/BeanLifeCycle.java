b3d4a598c340637e1aa56655a77ed1083c1cf9de
xie <858381526@qq.com>
2018-09-19 19:13:09 +0800

    初始化和销毁的第二种方法： 初始化：实现InitializingBean 销毁：实现DisposableBean

--- src/main/java/com/xie/study/annotation3/BeanLifeCycle.java
+++ src/main/java/com/xie/study/annotation3/BeanLifeCycle.java
@@ -2,6 +2,7 @@ package com.xie.study.annotation3;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,8 +12,13 @@ import org.springframework.context.annotation.Configuration;
  * 创建：单实例的bean在容器启动的时候调用，多实例的bean在该bean第一次使用的时候创建
  * 初始化：对象创建完成，并赋值（属性）好，调用初始化方法
  * 销毁：单例：容器关闭的时候。多例：容器不会管理这个bean,容器关闭也不会销毁。只能手动调用销毁方法
+ *
+ * 初始化和销毁的第二种方法：
+ *  初始化：实现InitializingBean，并使用它的afterPropertiesSet()方法
+ *  销毁：实现DisposableBean，并使用它的destroy()方法
  */
 @Configuration
+@ComponentScan("com.xie.study.annotation3")
 public class BeanLifeCycle {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext =new AnnotationConfigApplicationContext(BeanLifeCycle.class);
---
+++ src/main/java/com/xie/study/annotation3/Cat.java
@@ -0,0 +1,23 @@
+package com.xie.study.annotation3;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat implements InitializingBean,DisposableBean {
+
+    public Cat(){
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("cat 属性设置好后（也算一种初始化方法）");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("cat bean的销毁方法");
+    }
+}
