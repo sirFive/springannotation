2bc859eda0377a55a6714aa77b9928ea733ed94e
xie <858381526@qq.com>
2018-09-20 10:40:37 +0800

    spring底层对beanPostProcessor应用广泛

--- src/main/java/com/xie/study/annotation3/BeanPostProcessorAnnotation.java
+++ src/main/java/com/xie/study/annotation3/BeanPostProcessorAnnotation.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.Configuration;
  * BeanPostProcessor:在初始化的前后调用
  * 初始化前调用：postProcessBeforeInitialization，它比initMethod ，InitializingBean，@PostConstruct  还要早
  * 初始化后调用：postProcessAfterInitialization，它比initMethod ，InitializingBean，@PostConstruct 晚，但比destory()早
+ * spring底层对beanpostProcessor的使用：
+ *      applicationContext注入（ApplicationContextAwareProcessor）
+ *      bean校验（BeanValidationPostProcessor）
+ *      @Autowired（AutowiredAnnotationBeanPostProcessor）
+ *      bean赋值，注入其它组件，，生命周期注解功能等
  */
 @Configuration
 @ComponentScan("com.xie.study.annotation3")
