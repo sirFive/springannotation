aa498ce95a18a9a2d1c1b4395b5a28bf454adcda
xie <858381526@qq.com>
2018-09-25 18:43:42 +0800

    @Properties 加载外部配置文件

--- src/main/java/com/xie/study/annotation04/AnnotationProperty.java
+++ src/main/java/com/xie/study/annotation04/AnnotationProperty.java
@@ -1,12 +1,17 @@
 package com.xie.study.annotation04;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 属性赋值@Value
  * 1.基本数值       @Value("谢")
  * 2.spel 表达式   @Value("#{20+5}")  结果为：25
  * 3.取出配置文件中的值（在运行环境变量里面的值） @Value("${spring.xie.address}")
+ *
+ * 4.引入外部配置文件（在配置类中添加）：
+ *  @PropertySource(value = "classpath:/person.properties，classpath:/dog.properties")
+ * @Pr
  */
 public class AnnotationProperty {
 
